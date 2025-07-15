7792672e072caff1c8aa0c9e554bea016fe060b4
Francisco Castillo <castillof075@gmail.com>
2025-07-15 02:57:35 -0600

    https

--- src/settings/appsettings.js
+++ src/settings/appsettings.js
@@ -1,4 +1,4 @@
 export const appsettings = {
     //Aqui se debo de poner la ruta de la API
-    apiUrl:"http://localhost:5135/api/"
+    apiUrl:"https://localhost:5135/api/"
 }
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
+import fs from 'fs'
+
+
 
 export default defineConfig({
   plugins: [react()],
@@ -16,7 +19,10 @@ export default defineConfig({
     },
   },
   server: {
-    https: false,
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'localhost-key.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'localhost.pem')),
+    },
     port: 5173,
     host: true,
   },
