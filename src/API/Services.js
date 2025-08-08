f535301a5208ffde420aea3368bf96da758e52bf
FabianMolina <Geova@LAPTOP-3DL7BI1D>
2025-08-07 23:10:25 -0600

    arreglo de headers

--- src/API/Services.js
+++ src/API/Services.js
@@ -1,7 +1,14 @@
 import {appsettings} from '../settings/appsettings'
 
 export async function GetServices() {
-    const response = await fetch(`${appsettings.apiUrl}Services/GetServices`);
+    const response = await fetch(`${appsettings.apiUrl}Services/GetServices`
+
+        , {
+      method: 'GET',
+      credentials: 'include', 
+      headers: { Accept: 'application/json' }
+        }
+    );
     if(response.ok){
         const data = await response.json();
         return data;
